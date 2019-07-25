41d15c556210573e062250f5fd3a795d15125a01
keac <admin@wgpsec.org>
2019-07-25 18:07:14 +0800

    add mybatis

--- src/main/java/com/keac/controller/GlobalExceptionHandler.java
+++ src/main/java/com/keac/controller/GlobalExceptionHandler.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.HashMap;
 import java.util.Map;
 
-//全局捕获异常
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(RuntimeException.class)
-    @ResponseBody
-    public Map<String, Object> resultError(){
-        Map<String , Object> result = new HashMap<String, Object>();
-        result.put("errorCode", "500");
-        result.put("errorMsg", "系统错误");
-        return result;
-    }
-
-}
+////全局捕获异常
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(RuntimeException.class)
+//    @ResponseBody
+//    public Map<String, Object> resultError(){
+//        Map<String , Object> result = new HashMap<String, Object>();
+//        result.put("errorCode", "500");
+//        result.put("errorMsg", "系统错误");
+//        return result;
+//    }
+//
+//}
